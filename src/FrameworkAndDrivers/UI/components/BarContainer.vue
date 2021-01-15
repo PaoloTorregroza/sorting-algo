5652551848d28f1e589118e1a572d6db8c642ebd
PaoloTorregroza <paolinskypdtn@outlook.com>
2021-01-15 15:26:09 -0500

    Bar and BarContainer components created
    Change style in index.html for background color and padding

---
+++ src/FrameworkAndDrivers/UI/components/BarContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bar-container">
+    <Bar :value="10" :state="'normal'" />
+    <Bar :value="50" :state="'normal'"/>
+    <Bar :value="90" :state="'normal'"/>
+    <Bar :value="10" :state="'normal'"/>
+    <Bar :value="40" :state="'selected'"/>
+    <Bar :value="100" :state="'selected'"/>
+    <Bar :value="500" :state="'normal'"/>
+    <Bar :value="120" :state="'normal'"/>
+    <Bar :value="110" :state="'normal'"/>
+    <Bar :value="10" :state="'normal'" />
+    <Bar :value="500" :state="'normal'"/>
+    <Bar :value="120" :state="'normal'"/>
+    <Bar :value="110" :state="'normal'"/>
+    <Bar :value="140" :state="'done'"/>
+    <Bar :value="540" :state="'done'"/>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Bar from '../items/Bar.vue';
+
+@Component({
+  components: {
+    Bar,
+  },
+})
+export default class BarContainer extends Vue {}
+</script>
+
+<style scoped>
+.bar-container {
+    height: 85vh;
+    display: flex;
+    align-items: flex-end;
+    background-color: #3B4252;
+    padding: 15px;
+    padding-left: 12%;
+    padding-right: 12%;
+}
+</style>
