5c99b9a60847ad6d0097b61ce748bf5983afdd57
ElirethDev <pablosolerjs@gmail.com>
2021-02-12 16:23:34 -0400

    Insertion sort refactor to add new object  return with steps and values changes when sort algoritm happens

--- src/UseCases/InsertionSort.ts
+++ src/UseCases/InsertionSort.ts
@@ -1,7 +1,12 @@
+import Step from '@/Domain/Step';
+import createStepsArr from '../Utils/createStepsArr';
 
-function InsertionSort(list: number[] ) {
+function InsertionSort(list: number[]): Step[][] {
+    const res = [];
 
     const n = list.length;
+    const readyValues: number[] = [];
+    let changingValues: number[] = [];
 
     for (let i = 0; i < n; i++) {
         
@@ -9,15 +14,21 @@ function InsertionSort(list: number[] ) {
         const valor: number = list[i]
         while (pos > 1 && valor < list[pos - 1]) {
 
-            //cambio de lugares
             list[pos] = list[pos - 1]
+            changingValues = [pos - 1, valor];
+            res.push(createStepsArr(list, readyValues, changingValues))
             pos = pos - 1
 
         }
 
         list[pos] = valor
+
+        readyValues.push(n - 1 - pos)
+        res.push(createStepsArr(list, readyValues, changingValues))
         
     }
+
+    return res;
 
 }
 
